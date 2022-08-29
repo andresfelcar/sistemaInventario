dedc22560170ad141af79823bbe999eadeb44eef
Andres Caro <56310711+andresfelcar@users.noreply.github.com>
2022-08-28 19:06:13 -0500

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion{
 
 	public static function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=inventario",
+		$link = new PDO("mysql:host=u306480249_inventario;dbname=u306480249_inventario",
 			            "root",
-			            "");
+			            "~AF~FS:td3");
 
 		$link->exec("set names utf8");
 
